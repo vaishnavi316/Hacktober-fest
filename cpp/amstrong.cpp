e89079fe05115e02ec6d6dd20db902a68cd0e31e
anjali-01 <69755645+anjali-01@users.noreply.github.com>
2020-10-02 16:49:06 +0530

    Create amstrong.cpp

---
+++ cpp/amstrong.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int num, original, remainder, result = 0;
+    cout << "Enter a three-digit integer: ";
+    cin >> num;
+    original = num;
+
+    while (original != 0) 
+    {
+        remainder = original % 10;
+        
+        result += remainder * remainder * remainder;
+        original/= 10;
+    }
+
+    if (result == num)
+        cout << num << " is an Armstrong number.";
+    else
+        cout << num << " is not an Armstrong number.";
+
+    return 0;
+}
